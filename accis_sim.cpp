0f54f4f77c9e4a212f4e1e687323911c44199d7d
Zvonimir Stojanovski <zs66@cornell.edu>
2021-09-15 14:52:11 -0400

    Added state & distribution initialization

--- accis_sim.cpp
+++ accis_sim.cpp
@@ -7,11 +7,33 @@ void run_accis_sim(const time_info& t_info, filter::base& filt,
         const std::vector<sat_state>& init_ideal_state,
         sat_cam& camera, sat_state_randomizer& rzer) {
 
+    // Number of satellites
+    int num_sat = init_ideal_state.size();
+
     // Random number generator
     rando rnd(trial_no);
 
     // Set epoch
-    roamps_("T0", &t_info.start_time);
+    roamps_("T0", &t_info.start_time); 
+
+    // Initialize true state
+    std::vector<std::vector<sat_state>> state_tru(num_sat);
+    for (int i = 0; i < num_sat; i++)
+        state_tru[i].push_back(rzer.randomize(init_ideal_state[i], rnd));
+
+    // Initialize state estimate distributions
+    filter::dist dist_x0(sat_state::N);
+    dist_x0.cov = rzer.cov();
+    vec<> state_skew(sat_state::N), state_kurt(sat_state::N);
+    state_skew.setZero();
+    state_kurt.setConstant(3);
+    dist_x0.par.push_back(state_skew);
+    dist_x0.par.push_back(state_kurt);
+    std::vector<std::vector<filter::dist>> state_est(num_sat);
+    for (int i = 0; i < num_sat; i++) {
+        dist_x0.mean = init_ideal_state[i].X;
+        state_est[i].push_back(dist_x0);
+    }
 
     // Iterate over time steps
     for (int k = 0; k <= t_info.num_steps; k++) {
