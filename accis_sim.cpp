a10d7c06e79ea70748158642a251c9ae3e5f5160
Zvonimir Stojanovski <zs66@cornell.edu>
2021-09-15 13:58:25 -0400

    Added skeleton time loop

--- accis_sim.cpp
+++ accis_sim.cpp
@@ -1,5 +1,7 @@
 #include "accis_sim.hpp"
 
+#include "roam.h"
+
 void run_accis_sim(const time_info& t_info, filter::base& filt,
         const std::string& filt_name, int trial_no,
         const std::vector<sat_state>& init_ideal_state,
@@ -8,6 +10,26 @@ void run_accis_sim(const time_info& t_info, filter::base& filt,
     // Random number generator
     rando rnd(trial_no);
 
-    
+    // Set epoch
+    roamps_("T0", &t_info.start_time);
+
+    // Iterate over time steps
+    for (int k = 0; k <= t_info.num_steps; k++) {
+
+        // Propagate satellite states
+
+        // Filter prediction
+
+        // GPS Update
+        
+        // Star Tracker Update
+
+        // Acquire image
+
+        // Image-based update
+
+    }
+
+    // Save results
 
 }
