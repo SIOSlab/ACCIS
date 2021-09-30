fdb79a37268468a57d970a054beaee6c5ca46277
Zvonimir Stojanovski <zs66@cornell.edu>
2021-09-30 11:45:00 -0400

    Added printing step number

--- accis_run.py
+++ accis_run.py
@@ -1,5 +1,7 @@
 def accis_run(sat_list, num_steps) :
     for k in range(num_steps) :
+        if (k % 10 == 0) :
+            print('Step ' + str(k))
         for sat in sat_list :
             sat.step()
         for sat1 in sat_list :
