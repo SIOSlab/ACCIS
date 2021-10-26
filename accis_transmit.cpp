4029d3e5f519f8d8fc8c05e07ca51172d263b377
Zvonimir Stojanovski <zs66@cornell.edu>
2021-10-26 17:20:58 -0400

    Prohibited transmission from satellite to itself

--- accis_transmit.cpp
+++ accis_transmit.cpp
@@ -2,7 +2,7 @@
 
 void accis_sat::transmit(accis_sat& target) {
 
-    if (tr_last.step == step_no)
+    if (tr_last.step == step_no && target.sat_id != sat_id)
         target.cc.train.push_back(tr_last);
 
 }
