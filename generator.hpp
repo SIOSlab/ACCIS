fa12230a74ceeb1a61e400993c45990d31aae84b
Zvonimir Stojanovski <zs66@cornell.edu>
2022-05-08 15:13:01 -0400

    Added header file for generator

---
+++ generator.hpp
@@ -0,0 +1,25 @@
+#ifndef GENERATOR_HPP
+#define GENERATOR_HPP
+
+#include "sat_state.hpp"
+
+#include <string>
+
+class generator {
+
+    public:
+
+        run();
+
+    private:
+
+        int seed;
+            
+        double max_alt;
+        double min_alt;
+
+        sat_state_randomizer rzer;
+
+};
+
+#endif
