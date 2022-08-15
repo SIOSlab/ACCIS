98d03d22b8a293ea2776a0960f09bbe59c6259d1
ZvonimirS <zs66@cornell.edu>
2022-08-15 15:50:40 -0500

    Added exception handling for socket timeout

--- Landsat.java
+++ Landsat.java
@@ -6,6 +6,7 @@ import gov.nasa.worldwind.layers.Earth.LandsatI3WMSLayer;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 
 import javax.imageio.ImageIO;
 
@@ -33,45 +34,56 @@ public class Landsat {
             double dp) {
 
         // Flag
-        int flag;
+        int flag = 1;
         
         try {
 
-            // Initialize Landsat image layer
-            if (layer == null)
-                layer = new LandsatI3WMSLayer(); 
+            while (flag != 0) {
+            
+                try {
+
+                    // Initialize Landsat image layer
+                    if (layer == null)
+                        layer = new LandsatI3WMSLayer(); 
     
-            // Compute sector    
-            Sector sector = new Sector(Angle.fromDegrees(latc - lenp*dp),
+                    // Compute sector    
+                    Sector sector = new Sector(
+                                       Angle.fromDegrees(latc - lenp*dp),
                                        Angle.fromDegrees(latc),
                                        Angle.fromDegrees(lonc), 
                                        Angle.fromDegrees(lonc + widp*dp));
 
-            // Aspect ratio (pixel?)
-            double ar = 1; 
+                    // Aspect ratio (pixel?)
+                    double ar = 1; 
 
-            // Timeout (1 minute)
-            int timeout = 60000;
+                    // Timeout (1 minute)
+                    int timeout = 60000;
 
-            // Image type & filename 
-            String type = "image/png";
-            String filename = "landsat_temp.png";
+                    // Image type & filename 
+                    String type = "image/png";
+                    String filename = "landsat_temp.png";
 
-            // Acquire image 
-            BufferedImage img = layer.composeImageForSector(sector, widp, lenp, ar,
-                    -1, type, true, null, timeout);
+                    // Acquire image 
+                    BufferedImage img = layer.composeImageForSector(sector,
+                            widp, lenp, ar, -1, type, true, null, timeout);
 
-            // Save image
-            File outputfile = new File(filename);
-            ImageIO.write(img, "png", outputfile);
+                    // Save image
+                    File outputfile = new File(filename);
+                    ImageIO.write(img, "png", outputfile);
 
-            // Success
-            flag = 0;
+                    // Success
+                    flag = 0;
+
+                } catch (SocketTimeoutException ste) {
+
+                    System.out.println("Socket timed out -- Trying again");
+
+                }
+
+            }
 
         } catch (Exception e) {
 
-            // Failure
-            flag = 1; 
             e.printStackTrace(System.out);
         
         }
