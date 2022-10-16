229a78e44fb3ce8414e7ad6cf031ab95ba6b78ac
ZvonimirS <zs66@cornell.edu>
2022-10-16 14:21:37 -0400

    Added linear fitting

--- sift_fit.py
+++ sift_fit.py
@@ -2,10 +2,28 @@ import build.accis as accis
 
 from accis_io import *
 
-import numpy as np
+from numpy import *
 
 gen = accis_get_gen("gen")
 
 diffs = gen.get_diffs()
 
-np.savetxt("sift_coefs/diffs.csv", diffs, delimiter=",")
+npts = diffs.shape[0]
+ncol = diffs.shape[1]
+
+B = diffs[:, 0:4]
+A = diffs[:, 4:ncol]
+
+X, res, rank, s = linalg.lstsq(A, B, rcond=-1)
+
+savetxt("sift_coefs/h_mat.csv", transpose(X), delimiter=",")
+
+dY = B - matmul(A, X)
+
+R = matmul(transpose(dY), dY) / npts
+
+savetxt("sift_coefs/cov.csv", R, delimiter=",")
+
+bias = sum(dY, axis=0) / npts
+
+savetxt("sift_coefs/bias.csv", bias, delimiter=",")
