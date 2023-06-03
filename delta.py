2ac103583e7166388b0f232a57409741e5093d16
ZvonimirS <zs66@cornell.edu>
2023-06-03 19:57:13 -0400

    Adjusted phases in delta pattern & increased memory time

--- delta.py
+++ delta.py
@@ -1,14 +1,36 @@
+import math
+
 import build.accis as accis
 
 from accis_io import *
 from accis_run import *
 
+#-------------------------------------------------------------------------------
+
+n_plan = 4 
+n_phas = 1
+
+t_mins = 400
+
+t_save = 20
+
+steps_per_min = 120
+
+#-------------------------------------------------------------------------------
+
 sat_default = accis_get_sat('sat')
 
 par_default = sat_default.get_param()
 
-n_plan = 3
-n_phas = 4
+a = par_default["Orbit Semi-Major Axis (km)"]
+
+mu = 3.986E5;
+
+mean_mot = math.degrees(math.sqrt(mu / a**3))
+
+dt = 86400 / n_plan
+
+df = mean_mot * dt
 
 sats = []
 
@@ -18,7 +40,7 @@ for i in range(n_plan):
         par = par_default
 
         par["Orbit RAAN (deg)"]                  = i * 360.0 / n_plan
-        par["Orbit True Anomaly at Epoch (deg)"] = j * 360.0 / n_phas
+        par["Orbit True Anomaly at Epoch (deg)"] = j * 360.0 / n_phas - i * df
         par["Satellite ID"]                      = i * n_phas + j + 1
         par["Random Number Generator Seed"]      = j * n_plan + i + 1
 
@@ -27,12 +49,6 @@ for i in range(n_plan):
         sat.set_param(par)
 
         sats.append(sat)
-
-t_mins = 1000
-
-t_save = 10
-
-steps_per_min = 120
 
 steps = t_mins * steps_per_min
 save_cadence = t_save * steps_per_min
